128beac07a128bc79b31e9bee7d3488ed820a43a
Sherry <sheheryarghaznavi@yahoo.com>
2025-07-28 23:22:39 +0000

    lopp completed

--- Start/Ch2/loops_start.py
+++ Start/Ch2/loops_start.py
@@ -5,15 +5,47 @@
 x = 0
 
 # define a while loop
+while x > 5 :
+  print(x)
+  x = x + 1
+
+answer = input("Should i stop ? ")
+
+while answer != 'yes' :
+  print("answer is ", answer)
+  answer = input("Should i stop ? ")
 
 
 # define a for loop
+days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
 
+for d in days :
+  print(d)
+
+print('\n')
 
 # use a for loop over a collection
 
 
 # use the break and continue statements
+for d in days :
 
+  if d == 'thu' :
+    break
+  print(d)
+
+print('\n')
+
+
+for d in days :
+
+  if d == 'wed' :
+    continue
+  print(d)
+
+print('\n')
 
 # using the enumerate() function to get an index and an item
+for i, d in enumerate(days) :
+
+  print(i, d)
