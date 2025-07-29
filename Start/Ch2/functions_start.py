0b6444c610b9619108b965e0fbfaddf5f5bf9061
Sherry <sheheryarghaznavi@yahoo.com>
2025-07-29 00:00:50 +0000

    functions completed

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -3,17 +3,50 @@
 
 
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+
+def helloFunc() :
+  print("hello world!")
+  name = input("What is your name? ")
+  print("Nice to meet you,", name)
+
+# helloFunc()
 
 # function that takes parameters
+def helloFunc(greeting) :
+  print("hello world!")
+  name = input("What is your name? ")
+  print(greeting, name)
 
+# helloFunc("Nice to meet you too,")
 
 # function that returns a value
 
+def multiplyFunc(x) :
+  return x*x*x
+
+print(multiplyFunc(2))
 
 # function with default value for an parameter
 
+def helloFunc(greeting, name = None) :
+  print("hello world!")
+
+  if name == None :
+    name = input("What is your name? ")
+  print(greeting, name)
+
+# helloFunc("Nice to meet you too,", 'Ahmed')
+# helloFunc(name = 'Ahmed 2', greeting = "Nice to meet you too,")
+
 
 # function with variable number of parameters
+
+def multiParamFunc(*params) :
+
+  result = 0
+  for d in params :
+    result += d
+
+  return result
+
+print(multiParamFunc(2,2,2,2,3,3,3,3,4))
