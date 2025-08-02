00623f1d3327f86f13851d73f4caf58d917e2856
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-03 01:34:45 +0500

    ch6 write file completed

--- Start/Ch6/writefile_start.py
+++ Start/Ch6/writefile_start.py
@@ -4,12 +4,21 @@
 
 
 # Open a file for writing and create it if it doesn't exist
-
+file = open("text-file.txt", "w+")
+print(file.write("this is first data"))
+print(file.write("\n"))
+print(file.write("this is second data"))
+print(file.close())
+print("\n")
 
 # Open the file for appending text to the end
-
+file = open("text-file.txt", "a+")
 
 # write some lines of data to the file
+print(file.write("\nthis is third data"))
+print(file.write("\n"))
+print(file.write("this is second 2 data"))
 
 
 # close the file when done
+print(file.close())
