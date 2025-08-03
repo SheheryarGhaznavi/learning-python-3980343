2391e9905ebdd2d7755e68864df8dae80053ad9c
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-04 02:51:51 +0500

    ch7 json data completed

--- Start/Ch7/jsondata_start.py
+++ Start/Ch7/jsondata_start.py
@@ -3,12 +3,19 @@
 #
 
 import urllib.request 
+import json
 
 
 # Open the URL and read the data
-
+web_url = urllib.request.urlopen("http://uselessfacts.jsph.pl/api/v2/facts/random")
+print("Result code :",web_url.getcode())
 
 # Read the JSON data from the source
+data = web_url.read()
+print(data)
 
 
 # Print the content of the 'text' field
+response_data = json.loads(data)
+print("\n")
+print(response_data['text'])
