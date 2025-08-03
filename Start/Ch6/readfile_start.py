05cc50fc2af3422f1d76919669690f88793bc0a3
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-03 21:23:13 +0500

    ch6 read file completed

--- Start/Ch6/readfile_start.py
+++ Start/Ch6/readfile_start.py
@@ -5,5 +5,15 @@
 
     
 # Open the file and read the contents
+file  = open('text-file.txt', "r")
+
+if file.mode == "r" :
+  # contents = file.read()
+  # print(contents)
+  # print("\n")
+  content_lines = file.readlines()
+  
+  for line in content_lines : 
+      print(line)
 
     # use the read() function to read the entire file
