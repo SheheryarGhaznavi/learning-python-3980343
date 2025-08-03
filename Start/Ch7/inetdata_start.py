6c287548a99192a7542baa1ad1bc2affb8fa33e6
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-04 02:45:53 +0500

    ch7 inetdata completed

--- Start/Ch7/inetdata_start.py
+++ Start/Ch7/inetdata_start.py
@@ -1,3 +1,9 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for retrieving data from the internet
 #
+
+import urllib.request
+
+web_url = urllib.request.urlopen("https://example.com/")
+print("Result code :",web_url.getcode())
+print(web_url.read())
